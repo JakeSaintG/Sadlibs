16f9c2961512cc08e6a62436cc053a18afbc42c2
BuildTools <unconfigured@null.spigotmc.org>
2020-09-14 02:56:31 -0400

    started SadLibs Project

---
+++ js/story.js
@@ -0,0 +1,16 @@
+
+// // Declared variables and capture input.
+
+
+
+// // const adjective = prompt("Please give me an adjective.")
+// // const adjective2 = prompt("One more adjective.")
+// // const curse = prompt("Now one more adjective but make it a cruse word. We are all adults here.")
+// // const verb = prompt("Now how about a verb?")
+// // const noun = prompt("Finally, give me a noun.")
+
+// // Combine the input with other words to create a story.
+// const story = `The ${adjective} nurse decided to try to become a ${adjective2} programmer before a ${curse} pandemic hit. Now he is going to have to ${verb} his ${noun} off to do this homework and work overtime...`
+
+// // Display the story as a <p> inside the <main> element.
+// document.querySelector('main').innerHTML = story;
