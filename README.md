d8304cd0248af80faac485becc222eb70c4a8186
BuildTools <unconfigured@null.spigotmc.org>
2020-10-27 09:07:52 -0400

    added README and added functional JS

---
+++ README.md
@@ -0,0 +1,5 @@
+### Sadlibs!
+
+## A practice in JavaScript and Forms
+
+This is a simple application to test JS based off of the Mad Libs variant that was made "popular" by Travis McElroy. Enjoy the goofy take on the popular Road-Trip Game!
--- js/story.js
+++ js/story.js
@@ -1,16 +1,18 @@
 
-// // Declared variables and capture input.
+function generateLib() {
+    const adjective = document.getElementById("adjective1").value;
+    console.log(adjective)
+    const adjective2 = document.getElementById("adjective2").value;
+    console.log(adjective2)
+    const curse = document.getElementById("curse").value;
+    console.log(curse)
+    const verb = document.getElementById("verb").value;
+    console.log(verb)
+    const noun = document.getElementById("noun").value;
+    console.log(noun)
 
 
-
-// // const adjective = prompt("Please give me an adjective.")
-// // const adjective2 = prompt("One more adjective.")
-// // const curse = prompt("Now one more adjective but make it a cruse word. We are all adults here.")
-// // const verb = prompt("Now how about a verb?")
-// // const noun = prompt("Finally, give me a noun.")
-
-// // Combine the input with other words to create a story.
-// const story = `The ${adjective} nurse decided to try to become a ${adjective2} programmer before a ${curse} pandemic hit. Now he is going to have to ${verb} his ${noun} off to do this homework and work overtime...`
-
-// // Display the story as a <p> inside the <main> element.
-// document.querySelector('main').innerHTML = story;
+    document.getElementById("generatedLib").innerHTML = `
+        <p>The ${adjective} nurse decided to try to become a ${adjective2}
+        programmer before a ${curse} pandemic hit. Now he is going to have to ${verb} his ${noun} off to do this homework and work overtime...</p>`
+    }
